4157d7b299d32c751f298346a88c3a7381e16824
vaiYodo1 <96335421+vaiYodo1@users.noreply.github.com>
2022-01-09 18:44:31 +0900

    Add files via upload

---
+++ UnityProject/Assets/First_Velocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First_Velocity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //	rigidbody.velocity = new Vector3 (0.0f,2.0f,0.0f);
+        GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/GetKey.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey("up"))
+		{
+			transform.Rotate(1f, 0f, 0f);
+		}
+		if (Input.GetKey("down"))
+		{
+			transform.Rotate(-1f, 0f, 0f);
+		}
+		if (Input.GetKey("right"))
+		{
+			transform.Rotate(0f, 0f, -1f);
+		}
+		if (Input.GetKey("left"))
+		{
+			transform.Rotate(0f, 0f, 1f);
+		}
+	}
+}
